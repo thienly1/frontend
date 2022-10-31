cc50d27f4fe52ae089876eb919cbd26735eb4da5
THI LY TA <thienlysph@gmail.com>
2022-10-31 23:47:47 +0100

    Move Js to file + buttons

---
+++ 4.javascript/index.js
@@ -0,0 +1,17 @@
+
+
+ //Access content
+ document.getElementById("hello")
+ //Modifying content
+ .innerHTML= "Hello Group 43";
+
+ document.getElementById("hello").style.fontSize= "30px";
+ document.getElementById("hello").style.color="red";
+
+ //Write
+ document.write("Hello Again!");
+
+ //Alert: popup
+// window.alert("Warning");
+
+console.log("Hello in Console");
